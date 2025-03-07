4c0d47b03acc516a8f0e42ed89df622ab413fe52
thavamani18 <thavamani.tamtree@gmail.com>
2025-03-07 19:31:38 +0530

    addition

--- sample.py
+++ sample.py
@@ -1,6 +1,11 @@
 #####  basic program
+# x=30
+# y=10
+# s=x*y
+# print("The multiplication of the values",s)
+# print("The multiplication of the values")
+
 x=30
 y=10
-s=x*y
-print("The multiplication of the values",s)
-print("The multiplication of the values")
+s=x+y
+print("The addition of the values",s)
