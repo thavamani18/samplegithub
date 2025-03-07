43a5453237fe399eb79c829157b70608161b1f7b
thavamani18 <thavamani.tamtree@gmail.com>
2025-03-07 19:36:36 +0530

    subtract

--- sample.py
+++ sample.py
@@ -5,7 +5,11 @@
 # print("The multiplication of the values",s)
 # print("The multiplication of the values")
 
+# x=30
+# y=10
+# s=x+y
+# print("The addition of the values",s)
 x=30
 y=10
-s=x+y
-print("The addition of the values",s)
+s=x-y
+print("The subtration of the values",s)
